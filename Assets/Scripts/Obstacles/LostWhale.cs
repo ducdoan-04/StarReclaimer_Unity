8299f927b76627fdb1ba410d5fabdc1505216cca
ducdoan-04 <hoducdoan2004@gmail.com>
2025-10-29 09:18:49 +0700

    complete game level 1: find lostWhale

--- Assets/Scripts/Obstacles/LostWhale.cs
+++ Assets/Scripts/Obstacles/LostWhale.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Whale : MonoBehaviour
+public class LostWhale : MonoBehaviour
 {
-
     void Update()
     {
         float moveX = (GameManager.Instance.worldSpeed * PlayerController.Instance.boost) * Time.deltaTime;
         transform.position += new Vector3(-moveX, 0);
         if (transform.position.x < -11){
             Destroy(gameObject);
+        }
+    } 
+       
+    private void OnTriggerEnter2D(Collider2D collider){
+        if (collider.gameObject.CompareTag("Player")){
+            SceneManager.LoadScene("GameplayLV1Complete");
         }
     }
 }
